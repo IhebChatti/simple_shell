c9bd3b6c9275ab9af183678ac41f37f52e62ee3e
ihebchatti <chatti.iheb93@gmail.com>
2020-04-05 18:54:49 +0100

    string parser

---
+++ tokenizer.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char **tokenizer(char *str)
+{
+
+	int i = 0;
+	char *token = NULL;
+	char  **tokens;
+	char *del = " \t\r\n\a";
+
+	tokens = malloc(sizeof(char *) * 64);
+	if (tokens == NULL)
+		return (NULL);
+	token = strtok(str, del);
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		i++;
+		token = strtok(NULL, del);
+	}
+	tokens[i] = NULL;
+	return (tokens);
+}
