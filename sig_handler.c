07cc96525026c13eca92916d38c220fa994668b8
ihebchatti <chatti.iheb93@gmail.com>
2020-04-08 22:25:13 +0100

    signal handler

---
+++ sig_handler.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+void signal_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 2);
+		write(STDOUT_FILENO, "$", 2);
+	}
+}
