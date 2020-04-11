3ed922b007296c10e367cc4b320e6a08f67b2f9a
ihebchatti <chatti.iheb93@gmail.com>
2020-04-11 17:30:55 +0100

    built ins function fix

--- built_ins.c
+++ built_ins.c
@@ -10,26 +10,55 @@
 
 int built_ins(char **args)
 {
-	int i = 0;
-	b_ins our_b_ins[] = {
-		{"exit", _abort},
-		{NULL, NULL}
-	};
+	int i, switch_args = 0;
+	char* cmds_list[3];
 
-	if (args[0] == NULL)
-		return (1);
-	while (our_b_ins[i].name != NULL)
+	cmds_list[0] = "exit";
+	cmds_list[1] = "cd";
+	cmds_list[2] = "help";
+
+	for (i = 0; i < 3; i++)
 	{
-		if (_strcmp(our_b_ins->name, args[0]) == 0)
-			return (our_b_ins[i].fun(args));
-		i++;
+		if (_strcmp(args[0], cmds_list[i]) == 0)
+		{
+			switch_args = i + 1;
+			break;
+		}
+	}
+	switch (switch_args)
+	{
+		case 1:
+			_abort(args);
+		case 2:
+			cd(args[1]);
+			return (1);
+		case 3:
+			help();
+			return (1);
+		default:
+			break;
 	}
 	return (0);
 }
 
 int _abort(char **args)
 {
-	free(args);
-	free(*args);
+	free_arr(args);
 	exit(0);
+}
+
+int cd(char *path)
+{
+	return(chdir(path));
+}
+
+int help(void)
+{
+	write(STDOUT_FILENO, "Simple Shell\n",14);
+	write(STDOUT_FILENO, "Help:\n",7);
+	write(STDOUT_FILENO, "Builtin commands:\n", 19);
+	write(STDOUT_FILENO, "cd: change current working directory\n", 38);
+	write(STDOUT_FILENO, "exit: exit shell\n", 18);
+	write(STDOUT_FILENO, "help: displays help\n", 21);
+	return (0);
 }
